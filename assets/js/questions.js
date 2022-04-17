9bcb258f5f94c4d994736781f170ba8d04efb538
Ken Sheridan <kensheridan86@gmail.com>
2022-04-17 14:32:13 +0000

    added questions array for the quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,124 @@
+//quiz questions
+let questions = [{
+    'question': 'How many nations were involved with building the ISS?',
+    'correct': '16',
+    'a': '12',
+    'b': '16'
+},
+{
+    'question': 'The ISS orbits Earth every....?',
+    'correct': '90mins',
+    'a': '70mins',
+    'b': '90mins'
+},
+{
+    'question': 'How many bathrooms are on the ISS?',
+    'correct': '2',
+    'a': '2',
+    'b': '3'
+},
+{
+    'question': 'What was the estimated cost of building the ISS?',
+    'correct': '120 Billion',
+    'a': '85 Billion',
+    'b': '120 Billion'
+},
+{
+    'question': 'Who has spent the most time living in space?',
+    'correct': 'Peggy Whitson',
+    'a': 'Peggy Whitson',
+    'b': 'Chris Hadfield'
+},
+{
+    'question': 'The most days spent living in space?',
+    'correct': '665 days',
+    'a': '420 days',
+    'b': '665 days'
+},
+{
+    'question': 'How many sunsets per day are seen from the ISS?',
+    'correct': '16',
+    'a': '12',
+    'b': '16'
+},
+{
+    'question': 'Number of craft that can connect to the ISS?',
+    'correct': '8',
+    'a': '5',
+    'b': '8'
+},
+{
+    'question': 'In what year did the ISS launch?',
+    'correct': '1998',
+    'a': '1998',
+    'b': '2001'
+},
+{
+    'question': 'Which country is banned from visiting the ISS?',
+    'correct': 'China',
+    'a': 'China',
+    'b': 'Russia'
+},
+{
+    'question': 'Total weight of the ISS in Kgs is?',
+    'correct': '419725',
+    'a': '379495',
+    'b': '419725'
+},
+{
+    'question': 'The ISS has been continuously occupied since? ',
+    'correct': 'Nov 2000',
+    'a': 'Oct 1999',
+    'b': 'Nov 2000'
+},
+{
+    'question': 'How long is the ISS?',
+    'correct': '109 metres',
+    'a': '109 metres',
+    'b': '122 metres'
+},
+{
+    'question': 'Total lines of code written to run the ISS?',
+    'correct': '4.5 million',
+    'a': '4.5 million',
+    'b': '6.5 million'
+},
+{
+    'question': 'How many miles of electrical cable are installed?',
+    'correct': '8',
+    'a': '6',
+    'b': '8'
+},
+{
+    'question': 'How many tourists have visited the ISS?',
+    'correct': '7',
+    'a': '0',
+    'b': '7'
+},
+{
+    'question': 'Strangest thing ever delivered to the ISS?',
+    'correct': 'Pizza',
+    'a': 'Pizza',
+    'b': 'Vodka'
+},
+{
+    'question': 'Journey time from Earth to the ISS is?',
+    'correct': '4 Hours',
+    'a': '4 Hours',
+    'b': '8 Hours'
+},
+{
+    'question': 'How many computers control the systems aboard?',
+    'correct': '50',
+    'a': '30',
+    'b': '50'
+},
+{
+    'question': 'Power is provided by how many solar arrays?',
+    'correct': '8',
+    'a': '8',
+    'b': '10'
+}
+];
+
+const reloadQuestions = [...questions];
