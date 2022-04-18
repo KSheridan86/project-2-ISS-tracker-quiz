37f4084a85f819f36389fd79f5601d600c586c63
Ken Sheridan <kensheridan86@gmail.com>
2022-04-18 21:48:34 +0000

    variables for all of the containers added to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ let totalScore = 0;
 let initial = 20;
 let randomNumber;
 
+// Containers 
+let mapBox = document.querySelector('#map-box');
+let quizBox = document.querySelector('#quiz-box');
+let infoBox = document.querySelector('#info-box');
+let infoInner = document.querySelector('#info-inner');
+let dataBox = document.querySelector('#data-box');
+let btnContainer = document.querySelector('#button-container');
